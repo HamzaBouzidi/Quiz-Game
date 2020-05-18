6a869e02be46154352ef201767708198da2cfe3a
Reddman99 <63825742+Reddman99@users.noreply.github.com>
2020-05-18 16:46:07 +0100

    Initial commit

---
+++ app/src/main/java/com/example/game/Controller/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.game.Controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.game.Model.User;
+import com.example.game.R;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mGreetingText;
+    private EditText mNameInput;
+    private Button mPlayButton;
+    private User mUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mGreetingText=findViewById(R.id.activity_main_text_txv);
+        mNameInput=findViewById(R.id.activity_main_edit_etx);
+        mPlayButton=findViewById(R.id.activity_main_play_btn);
+        mUser= new User();
+
+
+
+        mPlayButton.setEnabled(false);
+        mNameInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                mPlayButton.setEnabled(s.toString().length() >3);
+
+
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String firstname = mNameInput.getText().toString();
+                mUser.setFirstName(firstname);
+                Intent GameActivityIntent = new Intent(MainActivity.this , GameActivity.class);
+                startActivity(GameActivityIntent);
+            }
+        });
+
+
+    }
+
+
+    }
+
+
+
+
